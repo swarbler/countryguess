f48f48991c0ed0bf23654692ab4be77580ed46b1
sbird <sbird@disroot.org>
2024-12-20 17:45:40 +0800

    added main menu

--- countryguess.py
+++ countryguess.py
@@ -1,4 +1,8 @@
-import random
+import time, random, sys
+from colorama import Fore, Back
+from art import *
+
+#* declares country lists *#
 
 # african countries
 AFRICA = [
@@ -279,26 +283,121 @@ UNRECOGNISED_TERRITORIES_ALT = {
     'republic of artsakh': 'nagorno-karabakh',
 }
 
-userInput = input('type a country: ')
-if userInput in AFRICA or userInput in AFRICA_ALT:
-    print('this country is in AFRICA')
-if userInput in ASIA or userInput in ASIA_ALT:
-    print('this country is in ASIA')
-if userInput in EUROPE or userInput in EUROPE_ALT:
-    print('this country is in EUROPE')
-if userInput in NORTH_AMERICA or userInput in NORTH_AMERICA_ALT:
-    print('this country is in NORTH AMERICA')
-if userInput in SOUTH_AMERICA:
-    print('this country is in SOUTH AMERICA')
-if userInput in OCEANIA:
-    print('this country is in OCEANIA')
-if userInput in OBSERVER_STATES or userInput in OBSERVER_STATES_ALT:
-    print('this is a U.N. observer state')
-if userInput in OTHER_DISPUTED_TERRITORIES:
-    print('this is a disputed territory')
-if userInput in UNRECOGNISED_TERRITORIES or userInput in UNRECOGNISED_TERRITORIES_ALT:
-    print('this is an unrecognised territory')
-if userInput in WORLD or userInput in WORLD_ALT:
-    print('this is a U.N. recognised country')
-else:
-    print('it is not a U.N. recognised country')
+#* declares functions *#
+
+def warning_tag(param): return Back.RED + param + Back.RESET
+
+def call_error(param, errorType='none', minR=0, maxR=0):
+    """error message"""
+
+    # something went wrong (Fire Font-k)
+    print(Fore.RED)     
+    tprint('something went wrong', 'fire-font-s')                                                                                                        
+    # print('                           )    )                                           )                                   ')
+    # print('             )      (   ( /( ( /( (          (  (    (  (      (         ( /(   (  (    (                (  (   ')
+    # print(' (    (     (      ))\\  )\\()))\\()))\\   (     )\\))(   )\\))(    ))\\  (     )\\())  )\\))(   )(    (    (     )\\))(  ')
+    # print(' )\\   )\\    )\\  \' /((_)(_))/((_)\\((_)  )\\ ) ((_))\\  ((_)()\\  /((_) )\\ ) (_))/  ((_)()\\ (()\\   )\\   )\\ ) ((_))\\  ')
+    # print('((_) ((_) _((_)) (_))  | |_ | |(_)(_) _(_/(  (()(_) _(()((_)(_))  _(_/( | |_   _(()((_) ((_) ((_) _(_/(  (()(_) ')
+    # print('(_-</ _ \\| \'  \\()/ -_) |  _|| \' \\ | || \' \\))/ _` |  \\ V  V // -_)| \' \\))|  _|  \\ V  V /| \'_|/ _ \\| \' \\))/ _` |  ')
+    # print('/__/\\___/|_|_|_| \\___|  \\__||_||_||_||_||_| \\__, |   \\_/\\_/ \\___||_||_|  \\__|   \\_/\\_/ |_|  \\___/|_||_| \\__, |  ')
+    # print('                                            |___/                                                       |___/   ')
+    # print('')
+
+    err = warning_tag(param)
+
+    match errorType:
+        case 'subject':
+            print(f'{err} is not a valid subject. Please try again.')
+        case 'topic':
+            print(f'{err} is not a valid topic. Please try again.')
+        case 'does_not_exist':
+            print(f'{err} does not exist yet. Please try again later.')
+        case 'range':
+            print(f'{err} is not a valid input. You must input a number between {minR} and {maxR}. Please try again')
+        case _:
+            print(f'{err} is not a valid input. Please try again.')
+    print('')
+    input('~~> ')
+
+    print('\033c', end='') # clear terminal
+
+#* program *#
+
+while True:
+    print(Fore.GREEN, end='')
+    tprint('countryguess', 'soft')
+
+    print(Fore.BLUE)
+    print('Choose an option:')
+
+    print(Fore.GREEN)
+    print('~ countries of the world')
+    print('~ continents')
+    print(Fore.RED +    '   ~ africa')
+    print(Fore.CYAN +   '   ~ asia')
+    print(Fore.MAGENTA +   '   ~ europe')
+    print(Fore.YELLOW + '   ~ north america')
+    print(Fore.GREEN +   '   ~ south america')
+    print(Fore.BLUE +   '   ~ oceania')
+
+    print(Fore.MAGENTA)
+    print('~ options')
+
+    print(Fore.RED)
+    print('~ quit')
+
+    userInput = input('~~> ').lower()
+
+    match userInput:
+        case 'countries of the world' | 'countries' | 'world':
+
+            print('\033c', end='') # clear terminal
+        case 'africa':
+
+            print('\033c', end='') # clear terminal
+        case 'asia':
+
+            print('\033c', end='') # clear terminal
+        case 'europe':
+
+            print('\033c', end='') # clear terminal
+        case 'north america':
+
+            print('\033c', end='') # clear terminal
+        case 'south america':
+
+            print('\033c', end='') # clear terminal
+        case 'oceania':
+
+            print('\033c', end='') # clear terminal
+        case 'options' | 'settings':
+
+            print('\033c', end='') # clear terminal
+        case 'quit':
+            sys.exit(0)
+        case _: # invalid input
+            call_error(userInput)
+
+# userInput = input('type a country: ')
+# if userInput in AFRICA or userInput in AFRICA_ALT:
+#     print('this country is in AFRICA')
+# if userInput in ASIA or userInput in ASIA_ALT:
+#     print('this country is in ASIA')
+# if userInput in EUROPE or userInput in EUROPE_ALT:
+#     print('this country is in EUROPE')
+# if userInput in NORTH_AMERICA or userInput in NORTH_AMERICA_ALT:
+#     print('this country is in NORTH AMERICA')
+# if userInput in SOUTH_AMERICA:
+#     print('this country is in SOUTH AMERICA')
+# if userInput in OCEANIA:
+#     print('this country is in OCEANIA')
+# if userInput in OBSERVER_STATES or userInput in OBSERVER_STATES_ALT:
+#     print('this is a U.N. observer state')
+# if userInput in OTHER_DISPUTED_TERRITORIES:
+#     print('this is a disputed territory')
+# if userInput in UNRECOGNISED_TERRITORIES or userInput in UNRECOGNISED_TERRITORIES_ALT:
+#     print('this is an unrecognised territory')
+# if userInput in WORLD or userInput in WORLD_ALT:
+#     print('this is a U.N. recognised country')
+# else:
+#     print('it is not a U.N. recognised country')
